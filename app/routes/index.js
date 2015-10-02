e9425105322d76e705d2aa5e43a3f78ced9d2568
ccmxy <windrainicefire@gmail.com>
2015-10-02 12:35:06 -0700

    Added json

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,42 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+actions: {
+    saveQ() {
+      var params = {
+       inquiry: this.get('question'),
+     };
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+}
+
 });
+/*
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('rental');
+  },
+
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      // Some code for processing the update parameters for the rental
+      rental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});*/
