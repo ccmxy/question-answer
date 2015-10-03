7566a70baed6981361ab74402c2d49760134054c
ccmxy <windrainicefire@gmail.com>
2015-10-03 01:59:04 -0700

    fixed issue with questions dissappearing if their box was selected and nothing entered during edit

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -9,9 +9,9 @@ export default Ember.Component.extend({
 
     update(question) {
       var params = {
-        inquiry: this.get('inquiry'),
-        notes: this.get('notes'),
-        author: this.get('author'),
+        inquiry: this.get('inquiry')? this.get('inquiry') : question.inquiry.value,
+        notes: this.get('notes')? this.get('notes') : question.notes.value,
+        author: this.get('author') ? this.get('author') : question.author.value,
       };
       Object.keys(params).forEach(function(key) {
           if(params[key]!==undefined) {
