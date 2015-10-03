ae260e1f0e7b236a74a9678c63683e2275a0c923
ccmxy <windrainicefire@gmail.com>
2015-10-02 22:47:03 -0700

    improved layout

--- app/components/add-answer.js
+++ app/components/add-answer.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewQuestion: false,
+  //addNewQuestion: false,
   actions: {
     answerFormShow() {
       this.set('addAnswer', true);
